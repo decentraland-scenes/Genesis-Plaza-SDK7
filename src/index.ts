e8ec5b1c120eef3cc1f45cbb16023269dad6cb0e
William Caine <william.caineiii@gmail.com>
2023-04-27 12:44:20 -0400

    adding some of the triggers (ones for jukebox)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { engine, executeTask, Material } from '@dcl/sdk/ecs'
-import { Color4 } from '@dcl/sdk/math'
+import * as utils from '@dcl-sdk/utils'
+import { Color4, Vector3 } from '@dcl/sdk/math'
 import { addBuildings } from './modules/buildings'
 //import { placeDoors } from './modules/bar/doors'
 import { barPlatforms } from './modules/platforms'
 import { addCloudLobby } from './lobby/cloudLobby'
-import { placeJukeBox, setBarMusicOn } from './modules/bar/jukebox'
+import { lowerVolume, outOfBar, placeJukeBox, setBarMusicOff, setBarMusicOn } from './modules/bar/jukebox'
+import { addRepeatTrigger } from './modules/Utils'
+import { log } from './back-ports/backPorts'
 
 
 // export all the functions required to make the scene work
@@ -31,20 +34,23 @@ placeDoors()
 */
 barPlatforms()
 
-/*
+
 //TODO TAG:PORT-REIMPLEMENT-ME
 
+//TODO TAG:PORT-REIMPLEMENT-ME
+/*
 utils.setTimeout(20000, () => {
   if (!areNPCsAdded) {
     handleQuests()
     addBarNPCs()
   }
 })
-
+*/
+//TODO TAG:PORT-REIMPLEMENT-ME
 /// TRIGGER FOR STUFF OUTSIDE BAR
-
+/*
 utils.addOneTimeTrigger(
-  new utils.TriggerBoxShape(new Vector3(50, 25, 50), new Vector3(160, 10, 155)),
+  new utils.TriggerBoxShape(Vector3.create(50, 25, 50), Vector3.create(160, 10, 155)),
   {
     onCameraEnter: () => {
       //debugger
@@ -52,25 +58,28 @@ utils.addOneTimeTrigger(
     },
     onCameraExit: async () => {
       await lowerVolume()
-      outsideBar()
+      //outsideBar()////TODO TAG:PORT-REIMPLEMENT-ME
       log('stepped out')
     },
   }
 )
+*/
+
+//utils.triggers.enableDebugDraw(true)
 
 // proper bar interior
 addRepeatTrigger(
-  new Vector3(160, 50, 155),
-  new Vector3(50, 102, 50),
+  Vector3.create(160, 50, 155),
+  Vector3.create(50, 102, 50),
   () => {
     setBarMusicOn()
     log('went in')
   },
-  null,
+  undefined,
   false,
   () => {
     outOfBar()
-    endArtistTalk()
+    //endArtistTalk() //TODO TAG:PORT-REIMPLEMENT-ME
     lowerVolume()
     log('mid distance')
 
@@ -80,13 +89,13 @@ addRepeatTrigger(
 
 //outer perimeter
 addRepeatTrigger(
-  new Vector3(160, 30, 155),
-  new Vector3(75, 60, 75),
+  Vector3.create(160, 30, 155),
+  Vector3.create(75, 60, 75),
   () => {
     lowerVolume()
     log('got closer')
   },
-  null,
+  undefined,
   false,
   () => {
     setBarMusicOff()
@@ -95,9 +104,9 @@ addRepeatTrigger(
 )
 
 /// TRIGGERS AROUND PLAZA
-
+/*
 utils.addOneTimeTrigger(
-  new utils.TriggerBoxShape(new Vector3(2, 5, 305), new Vector3(0, 2, 160)),
+  new utils.TriggerBoxShape(Vector3.create(2, 5, 305), Vector3.create(0, 2, 160)),
   {
     onCameraEnter: () => {
       log('WEST BORDER')
@@ -107,7 +116,7 @@ utils.addOneTimeTrigger(
 )
 
 utils.addOneTimeTrigger(
-  new utils.TriggerBoxShape(new Vector3(2, 5, 320), new Vector3(320, 2, 155)),
+  new utils.TriggerBoxShape(Vector3.create(2, 5, 320), Vector3.create(320, 2, 155)),
   {
     onCameraEnter: () => {
       log('EAST BORDER')
@@ -117,7 +126,7 @@ utils.addOneTimeTrigger(
 )
 
 utils.addOneTimeTrigger(
-  new utils.TriggerBoxShape(new Vector3(320, 5, 2), new Vector3(165, 2, 0)),
+  new utils.TriggerBoxShape(Vector3.create(320, 5, 2), Vector3.create(165, 2, 0)),
   {
     onCameraEnter: () => {
       log('SOUTH BORDER')
@@ -127,7 +136,7 @@ utils.addOneTimeTrigger(
 )
 
 utils.addOneTimeTrigger(
-  new utils.TriggerBoxShape(new Vector3(320, 5, 2), new Vector3(155, 2, 300)),
+  new utils.TriggerBoxShape(Vector3.create(320, 5, 2), Vector3.create(155, 2, 300)),
   {
     onCameraEnter: () => {
       log('NORTH BORDER')
