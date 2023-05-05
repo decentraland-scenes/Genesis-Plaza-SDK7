ae88d18874cc9138473fe3ae109b56768a768cb3
AnisLahouar <38791856+AnisLahouar@users.noreply.github.com>
2023-05-05 19:42:40 +0100

    feat: import elements from sdk6
    Made necessary changes

---
+++ src/modules/stats/AnalyticsConfig.ts
@@ -0,0 +1,29 @@
+export const SKIP_ANALYTICS: boolean = false
+export const AnalyticsLogLabel: string = "AnalyticLogs"
+
+export const ANALYTICS_EVENT_KEYS = {
+  scene_element_visit: "scene_element_visit",
+  scene_element_event: "scene_element_event",
+  scene_quest_event: "scene_quest_event",
+}
+
+export const ANALYTICS_ELEMENTS_TYPES = {
+  region: "region",
+  npc: "npc",
+  interactable: "interactable",
+}
+
+export const ANALYTICS_ELEMENTS_IDS = {
+  cloud: "cloud",
+  bar: "bar",
+  doge: "doge",
+  atariMachine: "atariMachine",
+  jukeBox: "jukeBox",
+  beerMug: "beerMug",
+  eventsSlider: "eventsSlider",
+}
+
+export const ANALYTICS_GENERIC_EVENTS = {
+  start: "start",
+  end: "end"
+}
---
+++ src/modules/stats/segements.ts
@@ -0,0 +1,137 @@
+import { Transform, engine } from "@dcl/sdk/ecs";
+import { SKIP_ANALYTICS } from "./AnalyticsConfig";
+import { getUserData } from "~system/UserIdentity"
+import { getRealm } from "~system/Runtime"
+import { getWorldPosition } from "@dcl-sdk/utils";
+
+const sceneId: string = "genesis_plaza"
+const inSeconds: boolean = false
+
+var segment: Segment = null
+
+export function getSegment() {
+  if (segment) return segment;
+
+  segment = new Segment('CH4D0eauyRqTDYnqhI0jw1PVPLmaJENj')
+  return segment
+}
+
+export async function sendTrackOld(eventName: string,) {
+  const doc: any = {
+    sceneId: sceneId,
+    playTime: Date.now(),
+    exactPosition: getWorldPosition(engine.PlayerEntity),
+    position: Math.floor(getWorldPosition(engine.PlayerEntity).x / 16) + "," + Math.floor(getWorldPosition(engine.PlayerEntity).z / 16)
+  }
+  await getSegment().track(eventName, doc)
+}
+
+export async function sendTrack(trackEvent: string,
+  elementType: string,
+  elementId: string,
+  instance: string,
+  event: string,
+  selection: string,
+  durationTime: number) {
+
+
+  const doc: any = {
+    sceneId: sceneId,
+    spanId: instance,
+    elementType: elementType,
+    elementId: elementId,
+    event: event,
+    selection: selection,
+    durationTime: inSeconds ? durationTime * 0.001 : durationTime,
+
+    playTime: Date.now(),
+    exactPosition: getWorldPosition(engine.PlayerEntity),
+    position: Math.floor(getWorldPosition(engine.PlayerEntity).x / 16) + "," + Math.floor(getWorldPosition(engine.PlayerEntity).z / 16)
+  }
+  await getSegment().track(trackEvent, doc)
+}
+
+declare function btoa(soruce: string): string
+
+class Segment {
+
+  constructor(public readonly segmentKey: string) { }
+
+  async track(event: string, properties?: Record<string, any>) {
+    if (SKIP_ANALYTICS) return
+    const userData = await getUserData({})
+    if (!userData) {
+      console.log(`[ignored] track("${event}"${properties ? ', {...}' : ''}): missing userData`)
+      return
+    }
+
+    const data: SegemntTrack = {
+      messageId: messageId(),
+      anonymousId: userData.data.userId,
+      timestamp: new Date().toJSON(),
+      event,
+      properties: {
+        ...properties,
+      },
+      context: {
+        library: {
+          name: 'dcl-segment-gists',
+          version: '0.0.0-development'
+        }
+      }
+    }
+
+    try {
+
+      await fetch(`https://api.segment.io/v1/track`, {
+        method: 'POST',
+        headers: {
+          'authorization': 'Basic ' + btoa(this.segmentKey + ':'),
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(data)
+      })
+    } catch (err) {
+      console.log(`[error] track("${event}"${properties ? ', {...}' : ''}): ${(err as Error)?.message || err}`)
+    }
+  }
+}
+
+const MESSAGE_ID_DATA = 'abcdef0123456789'
+
+function randomChar() {
+  return MESSAGE_ID_DATA[Math.floor(Math.random() * MESSAGE_ID_DATA.length)]
+}
+
+function messageId() {
+  return `dcl-scene-` + [...Array(36)].map(() => randomChar()).join('')
+}
+
+/**
+ * SegemntTrack 
+ * @property {string} anonymousId - The anonymous ID of the user.
+ * @property {string} event - The name of the event.
+ * @property {string} timestamp - The time the event occurred.
+ * @property {string} messageId - A unique identifier for the message.
+ * @property properties 
+ * @property context 
+ */
+type SegemntTrack = {
+  anonymousId: string,
+  event: string,
+  timestamp: string
+  messageId: string
+  properties: Record<string, any>,
+  context: Record<string, any>,
+}
+
+
+
+
+const realm = await getRealm({});
+const isPreview = realm.realmInfo?.isPreview
+    //
+    // make sure users are matched together by the same "realm".
+    //
+//options.realm = realm?.realmInfo?.realmName;
+//options.userData = await getUserData({});
