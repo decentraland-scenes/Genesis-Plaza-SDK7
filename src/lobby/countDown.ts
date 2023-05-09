dc26892f382d20f4b486480ee12df2441491e57b
William Caine <william.caineiii@gmail.com>
2023-05-09 14:39:16 -0400

    fix: stop counter before starting again

--- src/lobby/countDown.ts
+++ src/lobby/countDown.ts
@@ -4,22 +4,25 @@ import * as utils from '@dcl-sdk/utils'
 export class CountDownUtil {
     timerId?: TimerId
     counter: number = -1
-    
-    incrementBy: number = 1000
+    incrementBy:number = -1
+    timeIntervalMS: number = 1000
 
     start(startFrom: number) {
+        console.log("CountDownUtil","start", "startFrom", startFrom, this.counter, this.timerId)
+        //stop any existing counter
+        this.stop()
         this.counter = startFrom
         const host = this
         this.timerId = utils.timers.setInterval(() => {
             host.tick()
-        }, this.incrementBy)
+        }, this.timeIntervalMS)
     }
     tick() {
         console.log("CountDownUtil","tick", this.counter, this.timerId)
         if(this.counter === undefined){
             throw new Error ("start counter must be defined")
         }
-        this.counter--
+        this.counter += this.incrementBy
         if(this.counter <= 0){
             this.stop()
         }
@@ -27,6 +30,7 @@ export class CountDownUtil {
     stop(){
         console.log("CountDownUtil","stop", this.counter, this.timerId)
         if(this.timerId !== undefined){
+            console.log("CountDownUtil","stop","called clearInterval", this.counter, this.timerId)
             utils.timers.clearInterval(this.timerId)
         }
     }
