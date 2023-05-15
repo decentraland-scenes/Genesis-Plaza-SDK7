0a2d1ec850dd8e405f9fab00202a3ec5bb171ed4
dzsunyec <dzsunyec@gmail.com>
2023-05-15 17:05:21 +0200

    datePanel separate file removed

--- src/lobby/subItems/datePanel.ts
+++
@@ -1,8 +0,0 @@
-import { Entity, Material, MeshRenderer, PBMaterial, PBMeshRenderer_PlaneMesh, Transform, TransformType, TransformTypeWithOptionals, VisibilityComponent, engine } from "@dcl/sdk/ecs"
-import { Vector3 } from "@dcl/sdk/math"
-
-export class DatePanel {
-    public entity:Entity   
-
-    
-}
