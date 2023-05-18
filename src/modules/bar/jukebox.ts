43ac51fed539d71f4f2455e8c274d49dec96d3a5
nobodysGitHub <91459536+nobodysGitHub@users.noreply.github.com>
2023-05-18 12:44:12 +0700

    jukebox placed correctly

--- src/modules/bar/jukebox.ts
+++ src/modules/bar/jukebox.ts
@@ -4,6 +4,7 @@ import { Quaternion, Vector3 } from '@dcl/sdk/math'
 import * as utils from '@dcl-sdk/utils'
 import { sceneMessageBus } from '../serverHandler'
 import { tutorialRunning } from '../../lobby/portalBeam'
+import { coreBuildingOffset } from '../../constants'
 
 
 export enum Radios {
@@ -52,7 +53,7 @@ export function placeJukeBox() {
   })
 
   Transform.createOrReplace(baseJukeBox, {
-    position: Vector3.create(16, 0, 32), 
+    position: Vector3.create(179 - coreBuildingOffset.x, 0, 144 - coreBuildingOffset.z), 
     rotation: Quaternion.fromEulerDegrees(0, -45, 0),
     scale: Vector3.create(0.75, 0.75, 0.75),
   })
