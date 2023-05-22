d0897dc727b7438ce96f7d8b285fd719f7430d52
William Caine <william.caineiii@gmail.com>
2023-05-22 17:01:07 -0400

    have to drop for trees :(

--- src/lobby/resources/globals.ts
+++ src/lobby/resources/globals.ts
@@ -1,13 +1,13 @@
 import { Vector3 } from "@dcl/sdk/math";
 
 
-export const coreBuildingOffset = Vector3.create(128, 0, 112)
+export const coreBuildingOffset = Vector3.create(8*16, 0, 7*16)
 
 export const ParcelCountX = 4
 export const ParcelCountZ = 5
 export const lobbyCenter = Vector3.create(160,0,150.22)
 export const lobbyHeightLegacy = 104.73
-export const lobbyHeight = 76
+export const lobbyHeight = 72 //shorten trees by 6 meters
 export const lobbyRadius = 14.8
 
 export interface NoArgCallBack {
