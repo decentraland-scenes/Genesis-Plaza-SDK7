db59e6730c827111cc650150d06a9faa073cb4d9
William Caine <william.caineiii@gmail.com>
2023-05-22 14:04:47 -0400

    adding parcel size constants

--- src/lobby/resources/globals.ts
+++ src/lobby/resources/globals.ts
@@ -3,6 +3,8 @@ import { Vector3 } from "@dcl/sdk/math";
 
 export const coreBuildingOffset = Vector3.create(128, 0, 112)
 
+export const ParcelCountX = 4
+export const ParcelCountZ = 5
 export const lobbyCenter = Vector3.create(160,0,150.22)
 export const lobbyHeightLegacy = 104.73
 export const lobbyHeight = 76
